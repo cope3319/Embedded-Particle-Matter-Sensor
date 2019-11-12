381691bc5918566624685a522b97d3b67fc9ecdf
cope3319 <peskin.12@gmail.com>
2019-11-12 16:36:39 -0700

    Update sps30.c

--- sps30.c
+++ sps30.c
@@ -8,8 +8,7 @@
 #define SPS_MEASURE_STOP       0x01 // Stop Measurement
 #define SPS_MEASURE_READ       0x03 // Read Measurement
 #define SPS_FAN_SPEED          0x40 // Read Fan Speed
-#define SPS_FANCLEAN_INT       0x80 // Fancleaning interval set
-#define SPS_FANCLEAN_INT_READ  0x00 // Read fancleaning interval
+#define SPS_FANCLEAN_INT       0x80 // Read/Write Auto Cleaning interval
 #define SPS_START_FANCLEAN     0x56 // Start fancleaning
 #define SPS_RESET              0xd3 // Reset the SPS30
 
