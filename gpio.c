6e85475529911481acefc53d95c5c01283796c46
KaninMcGuire <kaninmcguire@yahoo.com>
2019-12-01 17:57:38 -0700

    UPDATE

--- gpio.c
+++ gpio.c
@@ -5,6 +5,6 @@
 
 void config_uart_gpio(void){
     P3->DIR |= 0b0001100; //Direction out for pins 3.2/3.3
-    P2->SEL0 |= 0b00001100;//sets selection register value for P2.4
-    P2->SEL1 &= ~(0b00001100);//sets selection register value for P2.4
+    P3->SEL0 |= 0b00001100;//sets selection register value for P2.4     /changed p2 to p3
+    P3->SEL1 &= ~(0b00001100);//sets selection register value for P2.4
 }
--- sps30UART.c
+++ sps30UART.c
@@ -8,31 +8,32 @@
 #include "msp.h"
 
 /*
+changed all EUSCI_A0 to EUSCI_A2
    > Pin 3.2 - TX
    > Pin 3.3 - RX
 */
 int config_sps_uart(void){
-    EUSCI_A0->CTLW0 |= EUSCI_A_CTLW0_SWRST; // Setting software reset enable
+    EUSCI_A2->CTLW0 |= EUSCI_A_CTLW0_SWRST; // Setting software reset enable
 
     //uart settings config
-    EUSCI_A0->CTLW0 |= (EUSCI_A_CTLW0_MODE_0 |  // select UART mode
+    EUSCI_A2->CTLW0 |= (EUSCI_A_CTLW0_MODE_0 |  // select UART mode
                         EUSCI_A_CTLW0_UCSSEL_2); // select SMCLK
     //baud rate config
-    EUSCI_A0->BRW = 8; //clock prescaler
-    EUSCI_A0->MCTLW |= (EUSCI_A_MCTLW_BRS_MASK & 0xD600); //set secondModReg to 214 (0xD6)
+    EUSCI_A2->BRW = 8; //clock prescaler
+    EUSCI_A2->MCTLW |= (EUSCI_A_MCTLW_BRS_MASK & 0xD600); //set secondModReg to 214 (0xD6)
 
-    EUSCI_A0->CTLW0 &= ~(EUSCI_A_CTLW0_SWRST); // Clear software reset
+    EUSCI_A2->CTLW0 &= ~(EUSCI_A_CTLW0_SWRST); // Clear software reset
 
-    EUSCI_A0->IE |= 0b11; // Transmit, and Recieve interrupt enabled
-    EUSCI_A0->IFG = 0;      // Clear all EUSCI_A0 interrupts
+    EUSCI_A2->IE |= 0b11; // Transmit, and Recieve interrupt enabled
+    EUSCI_A2->IFG = 0;      // Clear all EUSCI_A2 interrupts
     return 0;
 }
 
 void sps_uart_send(uint16_t len, uint8_t *data){
     uint16_t count = 0;
     while(count <= len){
-        while(EUSCI_A0->IFG < 2); // wait for transmit interrupt flags to clear
-        EUSCI_A0->TXBUF = data[count]; //store data in TX buffer
+        while(EUSCI_A2->IFG < 2); // wait for transmit interrupt flags to clear
+        EUSCI_A2->TXBUF = data[count]; //store data in TX buffer
         count++;
     }
 }
@@ -41,8 +42,8 @@ uint8_t sps_uart_recieve(uint16_t max_data_len, uint8_t *data){
     uint16_t count = 0;
     uint8_t lastVar = 0;
     while((count < max_data_len)|((lastVar = 0x7E && count != 1))){
-        while(EUSCI_A0->IFG == 0b1); // wait for interrupt flags to clear
-        data[count] = EUSCI_A0->RXBUF;
+        while(EUSCI_A2->IFG == 0b1); // wait for interrupt flags to clear
+        data[count] = EUSCI_A2->RXBUF;
         lastVar = data[count]; //to check for indicator last bit
         count++;
     }
