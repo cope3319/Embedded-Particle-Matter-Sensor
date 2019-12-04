fb5c7dd87e0189d59f0bf1217f6cfe1e42090d96
KaninMcGuire <kaninmcguire@yahoo.com>
2019-12-04 11:11:54 -0700

    Combined

---
+++ GPIO.h
@@ -0,0 +1,23 @@
+/*
+ * GPIO.h
+ *
+ *  Created on: Nov 14, 2019
+ *      Author: Kanin
+ */
+
+#ifndef GPIO_H_
+#define GPIO_H_
+
+#include "LCD.h"
+#include "msp.h"
+#include <string.h>
+#include <stdio.h>
+#include "Timer.h"
+
+
+void SPS_PinConfig();
+void LCD_PinConfig();
+
+
+
+#endif /* GPIO_H_ */
---
+++ gpioo.h
@@ -0,0 +1,10 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+#include "sensirion_shdlc.h"
+#include "sps30UART.h"
+
+void config_uart_gpio(void);
+
+#endif // GPIO_H
+
